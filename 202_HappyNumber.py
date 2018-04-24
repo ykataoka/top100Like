e0b502b0fa0f3691ff36aeec8f148a315efcebf2
Yasuyuki Kataoka <email@example.com>
2018-04-23 23:59:40 -0700

    e-mail change

--- 202_HappyNumber.py
+++ 202_HappyNumber.py
@@ -1,59 +1,58 @@
 class Solution(object):
 
-    # Solution1 : Simply use the set()
+#    # Solution1 : Simply use the set()
+#    def isHappy(self, n):
+#        """
+#        :type n: int
+#        :rtype: bool
+#        """
+#        store = set()
+#
+#        while n not in store:
+#
+#            # check the ending condition
+#            if n == 1:
+#                return True
+#
+#            # add to store
+#            store.add(n)
+#
+#            # convert int -> string
+#            n_str = str(n)
+#            n = 0
+#            for c in n_str:
+#                n += int(c)**2
+#
+#        return False
+
+    # Solution2 : fast and slow to avoid the extra space
     def isHappy(self, n):
         """
         :type n: int
         :rtype: bool
         """
-        store = set()
+        slow = n
+        fast = n
+        while fast != 1 and slow != 1:
 
-        while n not in store:
+            # compute slow
+            slow = self.digitSquaredSum(slow)
+            
+            # compute fast
+            fast = self.digitSquaredSum(fast)
+            fast = self.digitSquaredSum(fast)
+            print('slow, fast:', slow, fast)
+            # check the cycle condition
+            if (slow != 1) and (slow == fast):
+                return False
 
-            # check the ending condition
-            if n == 1:
-                return True
-
-            # add to store
-            store.add(n)
-
-            # convert int -> string
-            n_str = str(n)
-            n = 0
-            for c in n_str:
-                n += int(c)**2
-
-        return False
-
-    # Solution2 : fast and slow to remove the 
-    def isHappy(self, n):
-        """
-        :type n: int
-        :rtype: bool
-        """
-        store = set()
-
-        while n not in store:
-
-            # check the ending condition
-            if n == 1:
-                return True
-
-            # add to store
-            store.add(n)
-
-            # convert int -> string
-            n_str = str(n)
-            n = 0
-            for c in n_str:
-                n += int(c)**2
-
-        return False
+        return True
 
 
     def digitSquaredSum(self, n):
-        sum = 0
+        sum_ = 0
         while n:
             tmp = n % 10
-            sum += tmp**2
-
+            sum_ += tmp**2
+            n = n / 10
+        return sum_
---
+++ 46_Permutations.py
@@ -0,0 +1,35 @@
+class Solution(object):
+#    def permute(self, nums):
+#        """
+#        :type nums: List[int]
+#        :rtype: List[List[int]]
+#        """
+#        # base case
+#        if nums == []:
+#            return
+#        
+#        # recursive
+#        ans = []
+#        for i, num in enumerate(nums):
+#            tmp = []
+#            tmp.append(num)
+#            tmp = tmp + self.permute(nums[:i]+nums[i+1:])
+#            ans.append(tmp)
+#        return ans
+
+    # Solution 2: DFS
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        self.dfs(nums, [], res)
+        return res
+
+    def dfs(self, nums, path, res):
+        if nums == []:
+            return path
+
+        for i in range(len(nums)):
+            self.dfs(nums[:i]+nums[i+1:], path+nums[i], res)
